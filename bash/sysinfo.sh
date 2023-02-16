87d315ac8cd27ce10119464d1ec97624e8cddfad
Heybroswhatsup <subashadhikari201@gmail.com>
2023-02-15 20:48:06 -0500

    Lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
-echo "FQDN: $(hostname)"		# This command line interprets the domain name .
-echo "Host Information:"		# All the info is printed by this command line.
-hostnamectl				# It is used to  give info about OS.
-echo "IP Address= $(hostname -I)"	# This command print the ip address of this machine.
-df -h / 				# It prints out the storage used in a file system.
+# This  script  demonstrate  how to get  OS name and version, ip address , free space in root file system and also domain name.
+FQDN=$(hostname -f)
+
+#Get  information about operating system- you could also use lsb_relase
+OS=$(lsb_release -ds)
+
+#This command gets you the IP Address of the device
+IP=$(hostname -I)
+
+#It prints the free disk space on the root filesystem
+Free_Space=$(df -h / | awk 'NR==2 {print $4}')
+
+cat << EOF
+
+Report for $(hostname)
+===============
+FQDN: $FQDN
+Operating System: $OS
+IP Address: $IP
+Root File System Free Space: $Free_Space
+===============
+
+EOF
+
+
